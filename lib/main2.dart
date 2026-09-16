411087bfc9fa27ec97d779239913b1fe7a435873
Flame-Wizard <tasinahasan7593@gmail.com>
2026-09-17 00:38:41 +0600

    main 2

---
+++ lib/main2.dart
@@ -0,0 +1,15 @@
+import 'package:campus_hub/routine_home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RoutineHome(),
+    );
+  }
+}
