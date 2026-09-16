60069ef1d8e3ffff5cff6bd8e7e50e92a00b299c
ASUS <abdullah.cse.00724205101047@aust.edu>
2026-09-16 21:55:54 +0600

    Add login.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-import 'student_dashboard.dart';
+import 'login.dart';
+import 'registerpage.dart';
 
-void main() {
+
+void main(){
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatelessWidget{
+
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DashBoard(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFF6C5CE7),
+        scaffoldBackgroundColor: const Color(0xFF6C5CE7),
+      ),
+      home: const LoginScreen(),
     );
   }
 }
-
-
-
